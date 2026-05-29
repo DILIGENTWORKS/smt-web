6963228b6161e587063292b781aee8d28a5cbc3c
Rajesh Pannirselvam <pankalraj@gmail.com>
2026-05-29 19:46:54 +0100

    Add sumup link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             <Link to="/contact" className="nav-link">Contact & Times</Link>
           </li>
           <li className="nav-item">
-            <a href="https://sumup.link/YOUR_SUMUP_LINK" target="_blank" rel="noopener noreferrer" className="donate-button">Donate</a>
+            <a href="https://pay.sumup.com/b2c/QP0H1UDY" target="_blank" rel="noopener noreferrer" className="donate-button">Donate</a>
           </li>
         </ul>
       </div>  
